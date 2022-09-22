77fb8eb5b9b86243bea543f7f58660a2eef1531a
Niklas Lochschmidt <nlochschmidt@gmail.com>
2022-09-22 21:29:36 +0000

    Module-less simple DI

---
+++ src/main/kotlin/simple/SimpleDI.kt
@@ -0,0 +1,25 @@
+class Singleton private constructor() {
+    companion object { val INSTANCE by lazy { Singleton() }}
+}
+
+class Bean(val singleton: Singleton = Singleton.INSTANCE)
+
+class Service(
+  val eagerSingleton: Singleton         = Singleton.INSTANCE,
+  val lazySingleton: Lazy<Singleton>    = lazy { Singleton.INSTANCE },
+  val bean: Bean                        = Bean(),
+  val beanProvider: () -> Bean          = { Bean() },
+) {
+    fun injectableMethod(param: String, methodBean: Bean = Bean()) {
+        param
+        methodBean
+        eagerSingleton
+        lazySingleton
+        bean
+        beanProvider
+    }
+}
+
+fun app(service: Service = Service()) {
+    service.injectableMethod("Hello")
+}
